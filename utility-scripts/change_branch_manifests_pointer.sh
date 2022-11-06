5b61d604d9645edea2f5d15a3e4b4e07822ba87f
MoOyeg <mojojoye@yahoo.com>
2022-11-06 00:38:03 -0400

    Updating to fix automated script error

--- utility-scripts/change_branch_manifests_pointer.sh
+++ utility-scripts/change_branch_manifests_pointer.sh
@@ -1,15 +1,19 @@
 #!/bin/bash 
 #Utility script to reset application subscription yamls, argo application yamls to match current branch
 #Will also make changes to resourcegroup file to my default
+#Script Requires GIT
 
 BRANCH=${1:-main}
 RESOURCEGROUP=${2:-resourcegroup-replaceme}
+REPO_NAME=$(git remote -v | grep fetch | grep -o -E "github.com[:/]+.* " | sed 's$:$/$g' | sed 's$\.git$$g')
 
 #Change All Subscriptions to Branch Current Branch
 grep -rli 'apps.open-cluster-management.io/git-branch:' * | grep yaml | xargs -i@ sed -E -i 's/^( *apps.open-cluster-management.io\/git-branch:) .*/\1 '${BRANCH}'/g' @
 
-#Change All ArgoCD Applications to Current Branch
-grep -rli 'targetRevision:' * | grep yaml | xargs -i@ sed -E -i 's/^( *targetRevision:) .*/\1 '${BRANCH}'/g' @
+#Change All ArgoCD Applications in our repo(only origin) to Current Branch
+
+grep -rli 'targetRevision:' * |  grep -rli ${REPO_NAME} | grep yaml | xargs -i@ sed -E -i 's/^( *targetRevision:) .*/\1 '${BRANCH}'/g' @
+
 
 #Reset resourcegroupname to default
 grep -rli 'resourceGroupName:' * | grep yaml | xargs -i@ sed -E -i 's/^( +)resourceGroupName: .*/\1resourceGroupName: '${RESOURCEGROUP}'/g' @
