b14498d80c2df6f26c9535191d3eb039b509ef84
MoOyeg <mojojoye@yahoo.com>
2022-11-06 22:31:55 -0500

    updates

--- utility-scripts/change_branch_manifests_pointer.sh
+++ utility-scripts/change_branch_manifests_pointer.sh
@@ -13,13 +13,11 @@ REPO_NAME=$(git remote -v | grep fetch | grep -o -E "github.com[:/]+.* " | sed '
 grep -rli 'apps.open-cluster-management.io/git-branch:' * | grep yaml | xargs -i@ sed -E -i 's/^( *apps.open-cluster-management.io\/git-branch:) .*/\1 '${BRANCH}'/g' @
 
 #Change All ArgoCD Applications in our repo(only origin) to Current Branch
-
 grep -rli 'targetRevision:' * |  grep -rli ${REPO_NAME} | grep yaml | xargs -i@ sed -E -i 's/^( *targetRevision:) .*/\1 '${BRANCH}'/g' @
 
-
-#if [ "${NONPOINTER_CHANGE}" = "yes" ]
-#then
-#    #Reset resourcegroupname to default
-#    echo "test"
-#    grep -rli 'resourceGroupName:' * | grep yaml | xargs -i@ sed -E -i 's/^( +)resourceGroupName: .*/\1resourceGroupName: '${RESOURCEGROUP}'/g' @
-#fi
+#Update RG only if required
+if [ "${NONPOINTER_CHANGE}" = "yes" ]
+then
+   #Reset resourcegroupname to default
+   grep -rli 'resourceGroupName:' * | grep yaml | xargs -i@ sed -E -i 's/^( +)resourceGroupName: .*/\1resourceGroupName: '${RESOURCEGROUP}'/g' @
+fi
